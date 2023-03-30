a94f2d25871004b20ef523b2830bd2e4cbbf44bc
Robin Blokland <Robinpeterb@icloud.com>
2023-03-30 10:25:13 +0200

    klanttype zoals uml

--- KlantType.java
+++ KlantType.java
@@ -1,4 +1,26 @@
 public class KlantType
 {
+    private String adres;
+    private int telefoonnummer;
 
+    public KlantType(String adres, int telefoonnummer) {
+        this.adres = adres;
+        this.telefoonnummer = telefoonnummer;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public int getTelefoonnummer() {
+        return telefoonnummer;
+    }
+
+    public void setTelefoonnummer(int telefoonnummer) {
+        this.telefoonnummer = telefoonnummer;
+    }
 }
