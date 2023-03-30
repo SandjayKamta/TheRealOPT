e694f33515fa221ae502873319ee59a6b8bde756
Robin Blokland <Robinpeterb@icloud.com>
2023-03-30 10:19:07 +0200

    klant en klantype class

---
+++ Klant.java
@@ -0,0 +1,26 @@
+public class Klant extends Gebruiker
+{
+    private String adres;
+    private int telefoonnummer;
+
+    public Klant(String naam, String email, String adres, int telefoonnummer) {
+        super(naam, email);
+        this.adres = adres;
+        this.telefoonnummer = telefoonnummer;
+    }
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public int getTelefoonnummer() {
+        return telefoonnummer;
+    }
+
+    public void setTelefoonnummer(int telefoonnummer) {
+        this.telefoonnummer = telefoonnummer;
+    }
+}
