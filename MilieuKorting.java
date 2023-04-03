1a679d7a23caa088f174b153bb92bb10e10b19f4
Icedev1 <benaissa.sayfeddine@gmail.com>
2023-04-03 15:04:22 +0200

    milleu

--- MilieuKorting.java
+++ MilieuKorting.java
@@ -2,6 +2,11 @@ public class MilieuKorting {
     protected int kortingPercentage;
     protected String beschrijving;
 
+    public MilieuKorting(int kortingPercentage, String beschrijving) {
+        this.kortingPercentage = kortingPercentage;
+        this.beschrijving = beschrijving;
+    }
+
     public int getKortingsPercentage() {return kortingPercentage;}
     public void setKortingsPercentage(int kortingsPercentage) {this.kortingPercentage = kortingsPercentage;}
     public String getBeschrijving() {return beschrijving;}
