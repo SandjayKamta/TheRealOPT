4a79d0e27a9c82ae4c5ed60fc72d9f71eae2680f
Icedev1 <benaissa.sayfeddine@gmail.com>
2023-04-03 14:24:19 +0200

    milleu

--- MillieuKorting.java
+++ MillieuKorting.java
@@ -1,2 +1,9 @@
 public class MillieuKorting {
+    protected int kortingsPercentage;
+    protected String beschrijving;
+
+    public int getKortingsPercentage() {return kortingsPercentage;}
+    public void setKortingsPercentage(int kortingsPercentage) {this.kortingsPercentage = kortingsPercentage;}
+    public String getBeschrijving() {return beschrijving;}
+    public void setBeschrijving(String beschrijving) {this.beschrijving = beschrijving;}
 }
